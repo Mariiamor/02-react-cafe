2854343b83ee9c7d74b2d53f18dac50f4516ac4c
Mariia Khanelia <mariiapiskorska@gmail.com>
2025-05-16 17:35:52 +0200

    done 5 steps

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+// src/main.tsx
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
+import './index.css';
+import "modern-normalize";
+// Глобальні стилі (додатково)
+import "./global.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
